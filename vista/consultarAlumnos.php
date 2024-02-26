3128f8c22b63b6f0cb6bc721316656b5777d35c1
LilLuKaK <srbylukasmiczrgmail.com>
2024-02-26 17:32:18 +0100

    volcar select a consultar alumnos

--- vista/consultarAlumnos.php
+++ vista/consultarAlumnos.php
@@ -52,8 +52,27 @@
                         <label for="dni">Ciclo formativo</label>
                         <div class="input">
                             <select name="ciclos" id="ciclos" class="ciclos" sizeof="3">
-                                <option value="DAM">DAM</option>
-                                <option value="DAW">DAW</option>
+                                <?php
+                                // Incluir la conexión y consulta a la base de datos
+                                include './modelo/conexion.php';
+
+                                // Realizar la consulta para obtener los nombres de los ciclos formativos
+                                $conn = ConexionBD::conectar();
+
+                                if ($conn) {
+                                    $stmt = $conn->query("SELECT * FROM ciclos_formativos");
+
+                                    // Verificar si se encontraron resultados
+                                    if ($stmt->rowCount() > 0) {
+                                        // Iterar sobre los resultados y mostrar las opciones del select
+                                        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                                            echo "<option value='" . $row['Nombre_Ciclo'] . "'>" . $row['Nombre_Ciclo'] . "</option>";
+                                        }
+                                    } else {
+                                        echo "<option value='' disabled>No se encontraron ciclos formativos.</option>";
+                                    }
+                                }
+                                ?>
                             </select>
                             <span class="material-symbols-sharp expand">expand_more</span>
                             <button id="last" class="search" name="buscarFP"><span class="material-symbols-sharp">manage_search</span></button>
