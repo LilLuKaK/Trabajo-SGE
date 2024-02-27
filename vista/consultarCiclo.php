f600e72f13ed6c421f13dbe7d68e4d87b4eba86d
Piero Lara <jpierolara@gmail.com>
2024-02-27 15:18:23 +0100

    Preparabdo consultas SQL

--- vista/consultarCiclo.php
+++ vista/consultarCiclo.php
@@ -27,6 +27,48 @@
                 <button id="buscarCiclo" class="search" name="buscarCiclo"><span class="material-symbols-sharp">manage_search</span></button>
                 <input type="hidden" value="buscarCiclo">
             </div> 
+
+/**
+El switch de mostrar los alumnos actualmente en prácticas debe empezar con estado desactivado
+La consulta SQL ha de ser: 
+Consulta que se ha de repetir por cada uno de los Ciclos Formativos
+SELECT COUNT(*)
+FROM ciclo_alumno
+WHERE ID_Ciclo_Formativo=(SELECT ID_Ciclo_Formativo 
+                          FROM ciclos_formativos
+                          WHERE Nombre_Ciclo='DAM');
+
+
+                          SELECT COUNT(*)
+FROM ciclo_alumno
+WHERE ID_Ciclo_Formativo=(SELECT ID_Ciclo_Formativo 
+                          FROM ciclos_formativos
+                          WHERE Nombre_Ciclo='DAM')
+AND ID_Alumno = (SELECT ID_Alumno
+                 FROM alumnos
+                 WHERE alumnos.Validez =1);
+*/
+            <div class="form__control">
+                    <label for="conPracticas">Mostrar los Alumnos actualmente en prácticas (<span class="si">verde</span> para "si" y <span class="no">rojo</span> para "no") </label>
+                    <div class="input">
+                        <label class="switch">
+                            <input type="checkbox">
+                            <span class="slider round"></span>
+                        </label>
+                        <button class="search"><span class="material-symbols-sharp">manage_search</span></button>
+                    </div>
+                </div>
+                <div class="form__control">
+                    <label for="validez">Validez (<span class="si">verde</span> para "si" y <span class="no">rojo</span> para "no") </label>
+                    <div class="input">
+                        <label class="switch">
+                            <input type="checkbox">
+                            <span class="slider round"></span>
+                        </label>
+                        <button class="search"><span class="material-symbols-sharp">manage_search</span></button>
+                    </div>
+                </div>
+                
             
             
         </div>
